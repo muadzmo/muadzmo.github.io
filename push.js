912ee5eb98a882e79d93bb7a07237cf85c200503
muadz <muadzmohamad13@gmail.com>
2020-07-21 22:40:44 +0700

    catatan perubahan

--- push.js
+++ push.js
@@ -1,4 +1,4 @@
-var webPush = require('web-push');
+let webPush = require('web-push');
      
 const vapidKeys = {
    "publicKey": "BGg8iDQpMFGEOF4WxT6a1xlZMMBbGW1Cic2HUJ-vTvERlEehG3jzZX-4nnLeo7o86z_CX_FCTF18U3Q8W0jbt8U",
@@ -11,16 +11,16 @@ webPush.setVapidDetails(
    vapidKeys.publicKey,
    vapidKeys.privateKey
 )
-var pushSubscription = {
+let pushSubscription = {
    "endpoint": "https://fcm.googleapis.com/fcm/send/ey_EljcjPKU:APA91bF2beaN9IaMgVW9yUBUOTUpdkKyDCVIFbf2y1cH40ZDhz2Tf8z7pI9zgX2oehWozhu6imKcBtOjAhK6jZMDBMKkc9wmSUv-45Rdz4QxdDnXfe1MKHscajEecXw-sjw1e0F_uy_l",
    "keys": {
        "p256dh": "BHa13mFaDsYOaMRooTsqI2+AZYeETKaJfvBIbisDJ641cAvEoWlkHn+sy71r79hPpHjx/B8wMJkW3IrAa7LBn48=",
        "auth": "s5TVmzVpsgrBR9MuzG+DXw=="
    }
 };
-var payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
+let payload = 'Selamat! Aplikasi Anda sudah dapat menerima push notifikasi!';
  
-var options = {
+let options = {
    gcmAPIKey: '484856945109',
    TTL: 60
 };
--- service-worker.js
+++ service-worker.js
@@ -1,99 +1,23 @@
-// const CACHE_NAME = "firstpwa-v2";
-// let urlsToCache = [
-//   "/",
-//   "/icon.png",
-//   "/index.html",
-//   "/manifest.json",
-//   "/nav.html",
-//   "/package.json",
-//   "/push.js",
-//   "/team.html",
-//   "/js/api.js",
-//   "/js/db.js",
-//   "/js/idb.js",
-//   "/js/materialize.min.js",
-//   "/js/nav.js",
-//   "/js/script.js",
-//   "/js/team.js",
-//   "/pages/home.html",
-//   "/pages/favteam.html",
-//   "/css/materialize.min.css",
-//   "/favicon/android-icon-144x144.png",
-//   "/favicon/android-icon-192x192.png",
-//   "/favicon/favicon-32x32.png",
-//   "/favicon/favicon-16x16.png",
-//   "/favicon/favicon-96x96.png",
-// ];
-
-// self.addEventListener("install", (event) =>{
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache)=> {
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", (event) =>{
-//   const base_url = "https://api.football-data.org/v2/";
-//   const header = {
-//     headers: {
-//       "X-Auth-Token": "b40c7816650a44638b508fc392f5dac6",
-//     },
-//   };
-
-//   if (event.request.url.indexOf(base_url) > -1) {
-//     event.respondWith(
-//       caches.open(CACHE_NAME).then((cache) => {
-//         return fetch(event.request).then((response) => {
-//           cache.put(event.request.url, response.clone());
-//           return response;
-//         })
-//       })
-//     );
-//   } else {
-//     event.respondWith(
-//       caches.match(event.request, { ignoreSearch: true }).then((response) => {
-//         return response || fetch (event.request);
-//       })
-//     )
-//   }
-// });
-
-// self.addEventListener("activate", (event) =>{
-//   event.waitUntil(
-//     caches.keys().then((cacheNames)=> {
-//       return Promise.all(
-//         cacheNames.map((cacheName)=> {
-//           if (cacheName != CACHE_NAME) {
-//             console.log("ServiceWorker: cache " + cacheName + " dihapus");
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-// });
-
-// self.addEventListener('push', function(event) {
-//   var body;
-//   if (event.data) {
-//     body = event.data.text();
-//   } else {
-//     body = 'Push message no payload';
-//   }
-//   var options = {
-//     body: body,
-//     icon: 'img/notification.png',
-//     vibrate: [100, 50, 100],
-//     data: {
-//       dateOfArrival: Date.now(),
-//       primaryKey: 1
-//     }
-//   };
-//   event.waitUntil(
-//     self.registration.showNotification('Push Notification', options)
-//   );
-// });
+self.addEventListener('push', function(event) {
+  var body;
+  if (event.data) {
+    body = event.data.text();
+  } else {
+    body = 'Push message no payload';
+  }
+  var options = {
+    body: body,
+    icon: 'img/notification.png',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notification', options)
+  );
+});
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js');
      
